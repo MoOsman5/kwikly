10e574b9e5bee912f90e601154d15ea9efc0b22a
MoOsman5 <mohamed1592000gmail.com>
2024-02-04 23:24:18 +0200

    add brand route

--- kwikly-back-end/routes/index.js
+++ kwikly-back-end/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const CategoryRoute = require("./CategoryRoute");
 const subCategoryRoute = require("./SubCategoryRoute");
+const BrandRoute = require("./BrandRoute");
+
 const Routes = express.Router();
 
 Routes.use("/category", CategoryRoute);
 Routes.use("/subCategory", subCategoryRoute);
+Routes.use("/brand", BrandRoute);
 
 module.exports = Routes;
