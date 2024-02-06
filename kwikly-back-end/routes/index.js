fbd74883719b8a4038071a3780346c8e0751e1a9
MoOsman5 <mohamed1592000gmail.com>
2024-02-06 21:37:07 +0200

    add product route

--- kwikly-back-end/routes/index.js
+++ kwikly-back-end/routes/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const CategoryRoute = require("./CategoryRoute");
 const subCategoryRoute = require("./SubCategoryRoute");
 const BrandRoute = require("./BrandRoute");
+const ProductRoute = require("./ProductRoute");
 
 const Routes = express.Router();
 
 Routes.use("/category", CategoryRoute);
 Routes.use("/subCategory", subCategoryRoute);
 Routes.use("/brand", BrandRoute);
+Routes.use("/product", ProductRoute);
+
 
 module.exports = Routes;
